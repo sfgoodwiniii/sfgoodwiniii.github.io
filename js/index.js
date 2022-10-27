4afd59522715bab2373e18c271587a84dd8fe081
Stanley Goodwin <sfg99709aktech@gmail.com>
2022-10-27 15:06:09 -0400

    New Index

---
+++ js/index.js
@@ -0,0 +1,22 @@
+const data = [
+    {
+        "title": "Generate Resume",
+        "about": "Generates a resume from json to latex.",
+        "url": "https://github.com/sfgoodwiniii/Gen-Resume-Python",
+        "img": "img/resume_generator.png",
+        "img-alt": "Picture of Resume"
+    }
+]
+
+
+const grid_element = document.getElementById("grid");
+var html;
+
+
+
+for (var datum in data) {
+    html = ""
+    datum["title"]
+}
+
+element.appendChild(tag);
