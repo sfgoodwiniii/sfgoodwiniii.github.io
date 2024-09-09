f242457c483997a3da03bc614249a69ac33973a2
Stanley Goodwin <github@staniel.dev>
2024-09-09 00:49:04 -0400

    Update Header

--- .templates/header/switchLanguage.js
+++ .templates/header/switchLanguage.js
@@ -1,17 +1,18 @@
 // Constants
-PARENT = "sdev-settings-lang";
-DEFAULT = "EN";
+PARENT = "sdev-header__language";
+DEFAULT_LANGUAGE = "EN";
+TEXT_HIGHLIGHT_CLASS = "text-highlight";
 loadLangStartup();
 
 // Page Startup
 function loadLangStartup() {
 
 	// Fetch the language from the local storage
-	const langID = fetchLocalStorage("lang", DEFAULT);
+	const langID = fetchLocalStorage("lang", DEFAULT_LANGUAGE);
 
 	// Highlight the stored language
 	const langHTML = fetchObjectHTML(PARENT, langID);
-		  langHTML.classList.add("text-highlight");
+		  langHTML.classList.add(TEXT_HIGHLIGHT_CLASS);
 
 	// Load the content for the language
 	loadLangContent(langID);
@@ -21,16 +22,16 @@ function loadLangStartup() {
 function switchLanguage(langID) {
 
 	// If the language is already highlighted, ignore the click
-	if (localStorage.getItem("lang") === langID) { return; }
+	if (fetchLocalStorage("lang") === langID) { return; }
 
 	// Remove the highlight from the previous language
 	const previousLangID = fetchLocalStorage("lang");
 	const previousLangHTML = fetchObjectHTML(PARENT, previousLangID);
-		  previousLangHTML.classList.remove("text-highlight");
+		  previousLangHTML.classList.remove(TEXT_HIGHLIGHT_CLASS);
 
 	// Highlight the clicked language
 	const langHTML = fetchObjectHTML(PARENT, langID);
-		  langHTML.classList.add("text-highlight");
+		  langHTML.classList.add(TEXT_HIGHLIGHT_CLASS);
 
 	// Save the selected language to the local storage
 	localStorage.setItem("lang", langID);
@@ -39,7 +40,7 @@ function switchLanguage(langID) {
 	loadLangContent(langID);
 }
 
-// Load the content for the language
+// [TODO Add Actual Code] Load the content for the language
 function loadLangContent(langID) {
 	const langIDtoContent = {
 		"EN": "English",
