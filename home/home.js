69b4f35645e6a8dafd27f8fe6eae89592832263c
Stanley Goodwin <github@staniel.dev>
2024-11-02 14:48:39 -0400

    Update home.js

--- home/home.js
+++
@@ -1,4 +0,0 @@
-// When the page is finished loading, add transition: var(--theme-transition); to the body element
-document.addEventListener("DOMContentLoaded", function() {
-  document.body.style.transition = "var(--theme-transition)";
-});
